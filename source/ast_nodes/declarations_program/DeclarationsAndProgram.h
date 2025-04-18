bb9d43f56c990ead3b4734dc57cefe8b49dc886d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 00:48:06 +0300

    Update DeclarationsAndProgram.h

--- source/ast_nodes/declarations_program/DeclarationsAndProgram.h
+++ source/ast_nodes/declarations_program/DeclarationsAndProgram.h
@@ -1,1 +1,26 @@
+#include "Node.h"
+#include "Statements.h"
+#pragma once
+
+struct FunctionDeclaration : public Node
+{
+public:
+
+    std::string name;
+    std::unique_ptr<BlockStatement> body;
+
+    FunctionDeclaration(std::string name, std::unique_ptr<BlockStatement> body);
+
+};
+
+struct Program: public Node 
+{
+public:
+
+    std::vector<std::unique_ptr<FunctionDeclaration>> functions;
+
+    Program();
+
+};
+
 
