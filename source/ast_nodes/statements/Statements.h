850d6dabccee745ba1cf983ab9dda3f3288e6792
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 00:45:29 +0300

    Create Statements.h

---
+++ source/ast_nodes/statements/Statements.h
@@ -0,0 +1,49 @@
+#include "Node.h"
+#include "Expressions.h"
+#pragma once
+
+struct Statement : Node { using Node::Node; };
+
+struct ExpressionStatement: public Statement 
+{
+public:
+
+    std::unique_ptr<Expression> expression;
+
+    ExpressionStatement(std::unique_ptr<Expression> expression);
+
+};
+
+struct ReturnStatement: public Statement 
+{
+public:
+
+    std::unique_ptr<Expression> value;
+
+    ReturnStatement(std::unique_ptr<Expression> value);
+
+};
+
+struct IfStatement: public Statement 
+{
+public:
+
+    std::unique_ptr<Expression> condition;
+    std::unique_ptr<Statement> thenConsequence;
+    std::unique_ptr<Statement> elseConsequence;
+
+    IfStatement(std::unique_ptr<Expression> condition, 
+        std::unique_ptr<Statement> thenC, std::unique_ptr<Statement> elseC);
+
+};
+
+struct BlockStatement: public Statement
+{
+public:
+
+    std::vector<std::unique_ptr<Statement>> statements;
+
+    BlockStatement();
+
+};
+
