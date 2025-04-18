7c6c5825e415bb7fa440e58dcadb9ee488310072
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 02:35:54 +0300

    Create CallExpressionAndVariables.h

---
+++ source/ast_nodes/CallExpressionAndVariables/CallExpressionAndVariables.h
@@ -0,0 +1,38 @@
+#include "Statements.h"
+#pragma once
+
+enum class VariableType: uint8_t { Int, Float, Char };
+
+struct VariableDeclaration: public Statement 
+{
+public:
+
+    std::string name;
+    VariableType type;
+    std::unique_ptr<Expression> init;  
+
+    VariableDeclaration(const std::string& name, VariableType type,
+        std::unique_ptr<Expression> init);
+
+};
+
+struct Parameter 
+{
+public:
+
+    std::string name;
+    VariableType type;
+
+};
+
+struct CallExpression: public Expression
+{
+public:
+
+    std::string callee;
+    std::vector<std::unique_ptr<Expression>> arguments;
+
+    CallExpression(const std::string& callee,
+        std::vector<std::unique_ptr<Expression>> arguments);
+
+};
