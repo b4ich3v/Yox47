9f17345d513a5f99c2194a3a612688d1d05ea706
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 02:38:38 +0300

    Update Statements.cpp

--- source/ast_nodes/statements/Statements.cpp
+++ source/ast_nodes/statements/Statements.cpp
@@ -1,13 +1,13 @@
 #include "Statements.h"
 
-ExpressionStatement::ExpressionStatement(std::unique_ptr<Expression> expression): 
-	Statement(NodeType::EXPRESSION_STATMENT), expression(std::move(expression)) {}
+ExpressionStatement::ExpressionStatement(std::unique_ptr<Expression> expression) :
+	Statement(NodeType::EXPRESSION_STATEMENT), expression(std::move(expression)) {}
 
-ReturnStatement::ReturnStatement(std::unique_ptr<Expression> value): 
+ReturnStatement::ReturnStatement(std::unique_ptr<Expression> value) :
 	Statement(NodeType::RETURN_STATEMENT), value(std::move(value)) {}
 
-IfStatement::IfStatement(std::unique_ptr<Expression> condition, 
-	std::unique_ptr<Statement> thenC, std::unique_ptr<Statement> elseC):
+IfStatement::IfStatement(std::unique_ptr<Expression> condition,
+	std::unique_ptr<Statement> thenC, std::unique_ptr<Statement> elseC) :
 	Statement(NodeType::IF_STATEMENT), condition(std::move(condition)), thenConsequence(std::move(thenC)), elseConsequence(std::move(elseC)) {}
 
-BlockStatement::BlockStatement(): Statement(NodeType::BLOCK_STATMENT) {}
+BlockStatement::BlockStatement() : Statement(NodeType::BLOCK_STATEMENT) {}
