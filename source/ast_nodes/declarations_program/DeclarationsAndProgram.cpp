628fac342cc1378916abb394db7bb95cc142e204
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 00:48:27 +0300

    Update DeclarationsAndProgram.cpp

--- source/ast_nodes/declarations_program/DeclarationsAndProgram.cpp
+++ source/ast_nodes/declarations_program/DeclarationsAndProgram.cpp
@@ -1,1 +1,7 @@
+#include "DeclarationsAndProgram.h"
 
+FunctionDeclaration::FunctionDeclaration(std::string name, 
+	std::unique_ptr<BlockStatement> body):
+	Node(NodeType::FUNCTION_DECLARATION), name(name), body(std::move(body)) {}
+
+Program::Program(): Node(NodeType::PROGRAM) {}
