703c05ee831c19c6f2ffe8b3612ca336be897f8f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 00:50:07 +0300

    Update Parser.h

--- source/parser_lexer/Parser/Parser.h
+++ source/parser_lexer/Parser/Parser.h
@@ -1,1 +1,44 @@
+#include "Lexer.h"
+#include "DeclarationsAndProgram.h"
+#include "Statements.h"
+#include "Expressions.h"
+#pragma once
 
+class Parser
+{
+private:
+
+    Lexer& lexer;
+    Token currentToken;
+
+    void process();
+
+    bool checkToken(TokenType type) const;
+
+    bool matcher(TokenType type);
+
+    void expectManagement(TokenType type, const char* message);
+
+    static int priorityManagement(TokenType type);
+
+    std::unique_ptr<FunctionDeclaration> parseFunction();
+
+    std::unique_ptr<BlockStatement> parseBlock();
+
+    std::unique_ptr<Statement> parseStatement();
+
+    std::unique_ptr<Statement> parseIf();
+
+    std::unique_ptr<Statement> parseReturn();
+
+    std::unique_ptr<Expression> parseExpression(int minPrecedence = 0);
+
+    std::unique_ptr<Expression> parsePrimary();
+
+public:
+
+    explicit Parser(Lexer& lexer);
+
+    std::unique_ptr<Program> parseProgram();
+
+};
