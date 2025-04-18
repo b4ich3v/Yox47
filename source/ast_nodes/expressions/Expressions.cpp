6714164a2bdd993f0e73885e576003a7ba72adca
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-19 00:44:44 +0300

    Update Expressions.cpp

--- source/ast_nodes/expressions/Expressions.cpp
+++ source/ast_nodes/expressions/Expressions.cpp
@@ -1,1 +1,10 @@
+#include "Expressions.h"
 
+IdentifierExpression::IdentifierExpression(const std::string& name): Expression(NodeType::IDENTIFIER), name(name) {}
+
+IntegerLitExpression::IntegerLitExpression(const std::string& text): Expression(NodeType::INT_LITERAL), text(text) {}
+
+FloatLitExpression::FloatLitExpression(const std::string& text): Expression(NodeType::FLOAT_LITERAL), text(text) {}
+
+BinaryExpression::BinaryExpression(const TokenType& oper, std::unique_ptr<Expression> left, std::unique_ptr<Expression> right): 
+	Expression(NodeType::BINARY_EXPRESSION), oper(oper), left(std::move(left)), right(std::move(right)) {}
