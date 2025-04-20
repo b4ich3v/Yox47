8b78eddb73294989970172b2695c9e649b0e0312
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-21 00:15:55 +0300

    Update Semantic.h

--- source/semantic/Semantic.h
+++ source/semantic/Semantic.h
@@ -1,1 +1,83 @@
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include "Node.h"
+#include "CallExpressionAndVariables.h"
+#include "DeclarationsAndProgram.h"
+#pragma once
 
+struct VariableInfo
+{
+public:
+
+    VariableType type;
+
+};
+
+using Scope = std::unordered_map<std::string, VariableInfo>;
+
+class ScopedSymbolTable
+{
+private:
+
+    std::vector<Scope> stack{ 1 };
+
+public:
+
+    void enter();
+
+    void leave();
+
+    void declare(const std::string& name, VariableType type);
+
+    const VariableInfo* findVariable(const std::string& name) const;
+
+};
+
+class SemanticChecker
+{
+private:
+
+    ScopedSymbolTable symbols;
+
+    void visit(Node* node);
+
+    void visit(Program* program);
+
+    void visit(FunctionDeclaration* functionDeclaration);
+
+    void visit(BlockStatement* blockStatement);
+
+    void visit(VariableDeclaration* variableDeclaration);
+
+    void visit(ExpressionStatement* expressionStatement);
+
+    void visit(ReturnStatement* returnStatement);
+
+    void visit(IfStatement* ifStatement);
+
+    void visit(WhileStatement* whileStatement);
+
+    void visit(ForStatement* forStatement);
+
+    void visit(BreakStatement* breakStatement);
+
+    void visit(BoxLiteral* boxLiteral);
+
+    void visit(IndexExpression* indexExpression);
+
+    void visit(CallExpression* callExpression);
+
+    void visit(UnaryExpression* unaryExpression);
+
+    void visit(BinaryExpression* binaryExpression);
+
+    void visit(ChooseStatement* chooseStatement);
+
+    void visit(IdentifierExpression* id);
+
+public:
+
+    void check(Program* program);
+
+};
