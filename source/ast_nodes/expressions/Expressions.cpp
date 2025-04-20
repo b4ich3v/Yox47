36b3641789d60d3e1c58331242bc52d33009edf3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-21 00:08:19 +0300

    Update Expressions.cpp

--- source/ast_nodes/expressions/Expressions.cpp
+++ source/ast_nodes/expressions/Expressions.cpp
@@ -6,8 +6,21 @@ IntegerLitExpression::IntegerLitExpression(const std::string& text): Expression(
 
 FloatLitExpression::FloatLitExpression(const std::string& text): Expression(NodeType::FLOAT_LITERAL), text(text) {}
 
-BinaryExpression::BinaryExpression(const TokenType& oper, std::unique_ptr<Expression> left, std::unique_ptr<Expression> right): 
+BinaryExpression::BinaryExpression(const TokenType& oper, std::unique_ptr<Expression> left, std::unique_ptr<Expression> right):
 	Expression(NodeType::BINARY_EXPRESSION), oper(oper), left(std::move(left)), right(std::move(right)) {}
 
-CharLitExpression::CharLitExpression(const std::string& text)
-	: Expression(NodeType::CHAR_LITERAL), text(text) {}
+CharLitExpression::CharLitExpression(const std::string& text): 
+	Expression(NodeType::CHAR_LITERAL), text(text) {}
+
+BoolLitExpression::BoolLitExpression(bool value): 
+	Expression(NodeType::BOOL_LITERAL), value(value) {}
+
+UnaryExpression::UnaryExpression(TokenType oper, std::unique_ptr<Expression> expression): 
+	Expression(NodeType::UNARY_EXPRESSION), oper(oper), operand(std::move(expression)) {}
+
+BoxLiteral::BoxLiteral(std::vector<std::unique_ptr<Expression>> elements): 
+	Expression(NodeType::BOX_LITERAL), elements(std::move(elements)) {}
+
+IndexExpression::IndexExpression(std::unique_ptr<Expression> base,
+	std::unique_ptr<Expression> index): 
+	Expression(NodeType::INDEX_EXPRESSION), base(std::move(base)), index(std::move(index)) {}
