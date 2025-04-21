96a1307cfb7937775f2840baac28a6988c6ed796
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-21 15:04:50 +0300

    Update CodeGenerator.h

--- source/code_generator/CodeGenerator.h
+++ source/code_generator/CodeGenerator.h
@@ -1,1 +1,32 @@
+#include "DeclarationsAndProgram.h"
+#include "Expressions.h"
+#include "Statements.h"
+#include "CallExpressionAndVariables.h"
+#include <fstream>
+#pragma once
+
+class CodeGenerator 
+{
+private:
+
+    Program* root;
+    std::ofstream file;
+
+    void emit(const std::string& text);
+
+    void genProgram(Program* program);
+
+    void genFunction(FunctionDeclaration* functionDeclaration);
+
+    void genStatement(Statement* statement);
+
+    void genExpression(Expression* expression);
+
+public:
+
+    explicit CodeGenerator(Program* root, const std::string& fileName);
+
+    void generate();
+
+};
 
