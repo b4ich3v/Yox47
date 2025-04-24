3c7ad261c062da37e44357e1649d377452e994ff
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-24 18:23:09 +0300

    Update result.bat

--- source/result.bat
+++ source/result.bat
@@ -1,5 +1,5 @@
 nasm -f win64 runtime.asm -o runtime.o
-nasm -f win64 out.asm     -o out.o
+nasm -f win64 out.asm -o out.o
 gcc runtime.o out.o -o prog.exe -nostartfiles -Wl,--subsystem,console
 prog.exe
 pause
