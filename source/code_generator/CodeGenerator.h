9c7affb8a77215b2a0b3346ea46fea2161e55bda
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-27 22:44:20 +0300

    Update CodeGenerator.h

--- source/code_generator/CodeGenerator.h
+++ source/code_generator/CodeGenerator.h
@@ -2,6 +2,7 @@
 #include "Expressions.h"
 #include "Statements.h"
 #include "CallExpressionAndVariables.h"
+#include "Semantic.h"
 #include <fstream>
 #include <string>
 #include <unordered_map>
@@ -12,14 +13,21 @@ class CodeGenerator
 {
 private:
 
-    Program* root;
+    Program* root = nullptr;
     std::ofstream file;
+
+    std::unordered_map<std::string, VariableType> varTypes;
+    std::unordered_map<std::string, VariableType> fnReturnTypes;
     int currentStackOffset;
 
     std::stack<std::string> breakLabels;
     std::unordered_map<std::string, int> localOffsets;
     std::unordered_map<std::string, std::string> floatPool;
     
+    bool isFloatExpression(Expression* expression);
+
+    static std::string floatLabel(const std::string& txt);
+
     void generateLine(const std::string& text);
 
     void generateProgram(Program* program);
@@ -37,5 +45,7 @@ public:
     void generate();
 
     static const char* typeTag(NodeType type);
+
+    static std::string sanitize(const std::string& str);
 
 };
